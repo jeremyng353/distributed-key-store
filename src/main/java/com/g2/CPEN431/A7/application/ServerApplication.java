02a8a81c8c1e8933b4cdb7990660c8c910854fd3
nick-ng13 <nicholas_ng@shaw.ca>
2023-02-26 13:43:07 -0800

    push my a4 for init commit

--- src/main/java/com/g2/CPEN431/A7/application/ServerApplication.java
+++
@@ -1,8 +0,0 @@
-package com.g2.CPEN431.A7.application;
-
-import ca.NetSysLab.ProtocolBuffers.Message;
-
-public interface ServerApplication {
-    Message.Msg handleRequest(Message.Msg request);
-    Message.Msg generateAtCapacityError(Message.Msg request);
-}
--- src/main/java/com/g2/CPEN431/A7/cache/Cache.java
+++
@@ -1,8 +0,0 @@
-package com.g2.CPEN431.A7.cache;
-
-public interface Cache<K, V> {
-    void put(K key, V value);
-    V get(K key);
-    void invalidateAll();
-    boolean isFull();
-}
