63d3bac050af349e32ab35048f57ccb4925ce2c2
sammauns <maunsellsam@gmail.com>
2023-02-27 20:21:11 -0800

    added node ip funtionality to start.sh

--- start.sh
+++ start.sh
@@ -12,6 +12,15 @@ hupexit() {
 
 trap hupexit HUP
 trap cleanup INT
+# get public IP using curl and save it to a variable
+PUBLIC_IP=$(curl -s https://api.ipify.org/)
+truncate -s 0 nodes.txt
+
+for ((i = 4445; i < 4450; i++)); do
+    echo $PUBLIC_IP >> nodes.txt
+    echo $i >> nodes.txt
+
+done
 
 for ((i = 4445; i < 4450; i++)); do
     java -jar -Xmx64m A6.jar $i &
