587b69a7d9222bcd6328ce36b83da43e733ef104
Jeremy Ng <jeremyng353@gmail.com>
2023-03-01 22:43:07 -0800

    change to 5 nodes for a6

--- src/main/java/com/g2/CPEN431/A7/App.java
+++ src/main/java/com/g2/CPEN431/A7/App.java
@@ -68,7 +68,7 @@ public class App
 
                 Message.Msg message = Server.readRequest(packet);
 
-                Object kvResponse;
+                ByteString kvResponse;
                 // if message cached retrieved cached response otherwise execute command
                 if (RequestCache.isStored(message.getMessageID())) {
                     kvResponse = RequestCache.get(message.getMessageID());
@@ -93,23 +93,6 @@ public class App
                     packet = new DatagramPacket(resMessage, resMessage.length, address, packetPort);
                     socket.send(packet);
                 }
-
-                /*
-                if (kvResponse instanceof DatagramPacket) {
-                    packet = new DatagramPacket(((DatagramPacket) kvResponse).getData(), ((DatagramPacket) kvResponse).getLength(), address, packetPort);
-                    socket.send(packet);
-                } else if (kvResponse instanceof ByteString) {
-                    // build checksum and response message
-                    long checksum = Server.buildChecksum(message.getMessageID(), (ByteString) kvResponse);
-                    byte[] resMessage = Server.buildMessage(message.getMessageID(), (ByteString) kvResponse, checksum);
-
-                    // load message into packet to send back to client
-                    packet = new DatagramPacket(resMessage, resMessage.length, address, packetPort);
-                    socket.send(packet);
-                }
-
-                 */
-
             } catch (PacketCorruptionException e) {
                 System.out.println("the packet is corrupt");
             }
